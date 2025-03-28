7f87e6d94ee76aa17a352e447d4ef2a667563928
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-03-27 20:38:08 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Mr. Coxall
+// Updated by: Lynn Lukose
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
 /**
- * Displays an alert with the message "Hello, World!"
+ * Do basic math.
  */
-function myButtonClicked() {
-  alert("Hello, World!")
+function enterClicked() {
+  // input
+  const streetNumber = parseInt(document.getElementById("street-number").value)
+  const streetName = document.getElementById("street-name").value
+  // output
+  document.getElementById("user-info").innerHTML =
+    "Your info is: " + streetNumber + ", " + streetName + "."
 }
